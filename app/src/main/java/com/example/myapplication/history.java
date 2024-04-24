ea5d76807bd597d1916b00ccc18c9d4bba92b59f
brlya <gekagekov1@gmail.com>
2024-04-24 22:51:53 +0700

    2

--- app/src/main/java/com/example/myapplication/history.java
+++ app/src/main/java/com/example/myapplication/history.java
@@ -2,42 +2,29 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Glavnaya extends AppCompatActivity   {
+public class history extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_glavnaya);
+        setContentView(R.layout.activity_history);
 
 
     }
-
-    public void gotohis(View v) {
-        startActivity(new Intent(Glavnaya.this, history.class));
-    }
-
     public void gotosett(View v) {
-        startActivity(new Intent(Glavnaya.this, Settings.class));
+        startActivity(new Intent(history.this, Settings.class));
     }
-
-
-
-
+    public void gotohom(View v) {
+        startActivity(new Intent(history.this, Glavnaya.class));
+    }
 }
-
-
-
