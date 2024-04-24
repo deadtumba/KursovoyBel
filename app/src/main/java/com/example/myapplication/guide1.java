9ed65d5a83011bb5fbde6367c0defad6adfcf67e
brlya <gekagekov1@gmail.com>
2024-04-24 19:43:47 +0700

    1

---
+++ app/src/main/java/com/example/myapplication/guide1.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class guide1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.guide1layout);
+        Button btndalee = (Button) findViewById(R.id.button);
+        btndalee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(guide1.this, guide2.class));
+            }
+        });
+    }
+
+}
